fbfd8576d2fabd193074c5330b453c45baea7dee
Eugene Zorin <ezorin5@yandex.ru>
2023-02-23 20:39:37 +0400

    card fix

--- src/components/components/MovieCard/MovieCard.js
+++ src/components/components/MovieCard/MovieCard.js
@@ -1,42 +1,45 @@
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
 import CardButton from '../../UI/CardButton/CardButton';
 import MovieRating from '../../UI/MovieRating/MovieRating';
-import LinkButton from '../../UI/LinkButton/LinkButton';
 import { POSTER_HORIZONTAL_SMALL } from '../../../utils/constants';
 import { routesConfig } from '../../../utils/configs';
+import posterFallback from '../../../images/poster_fallback.png';
 import './MovieCard.css';
 
 const MovieCard = memo(function MovieCard({ movie, place }) {
     return (
-        <figure className='movie-card' >
-            <div className='movie-card__poster-wrapper' >
-                <img
-                    src={POSTER_HORIZONTAL_SMALL + movie.poster_path}
-                    alt='постер фильма'
-                    className='movie-card__poster'
+        <div className='movie-card'>
+            <figure className='movie-card__figure' >
+                <div className='movie-card__poster-wrapper' >
+                    <img
+                        src={POSTER_HORIZONTAL_SMALL + movie.poster_path}
+                        alt='постер фильма'
+                        className='movie-card__poster'
+                        onError={(evt) => evt.target.src = posterFallback}
+                    />
+                </div>
+                <figcaption className='movie-card__caption' >
+                    <span className='movie-card__title' >
+                        {movie.title}
+                    </span>
+                </figcaption>
+            </figure>
+            <Link
+                to={routesConfig.movieInfo + '/' + movie.id}
+                className='movie-card__link-wrapper'
+            />
+            <div className='movie-card__rating-wrapper' >
+                <MovieRating
+                    rating={movie.vote_average}
                 />
             </div>
-            <figcaption className='movie-card__caption' >
-                <div className='movie-card__controls' >
-                    <MovieRating
-                        rating={movie.vote_average}
-                    />
-                    <CardButton
-                        isActive={movie.id % 2 === 0}
-                    />
-                </div>
-                <span className='movie-card__title' >
-                    {movie.title}
-                </span>
-                <div className='movie-card__link-button' >
-                    <LinkButton
-                        link={routesConfig.movieInfo + '/' + movie.id}
-                        place='movie-card'
-                        text='Подробнее'
-                    />
-                </div>
-            </figcaption>
-        </figure>
+            <div className='movie-card__button-wrapper' >
+                <CardButton
+                    isActive={movie.id % 3 === 0}
+                />
+            </div>
+        </div>
     );
 })
 
